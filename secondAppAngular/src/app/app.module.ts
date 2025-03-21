a7be2cd9f25bb7a96c5685ad1da419c998df3dd9
Bednarek <b.bednarek@accenture.com>
2025-03-21 11:26:22 +0100

    create first folder

---
+++ secondAppAngular/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProductFeatureModule } from './product-feature/product-feature.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    //ProductComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ProductFeatureModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ secondAppAngular/src/app/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+  //styles:[`h4{color:red; background-color: lightblue}` ]
+})
+export class HomeComponent {
+
+}
